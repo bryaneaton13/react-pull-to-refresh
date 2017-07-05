6e56ded84320333e1fe11df5d80f185a60590bcf
KasperN <kasper@taiste.fi>
2017-07-05 21:28:28 +0300

    Fix PropTypes warning from React (#16)
    * Fixed PropTypes warning from React
    
    * Update package.json to have prop-types

--- src/components/ReactPullToRefresh.js
+++ src/components/ReactPullToRefresh.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 import WebPullToRefresh from '../pull-to-refresh/wptr.1.1';
 
 export default class ReactPullToRefresh extends Component {
