f0e07ec55ebce71b906e84c7a6d50861c425e5e5
Bryan Eaton <bryaneaton13@gmail.com>
2023-03-22 10:11:07 -0400

    Update deps for playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-	plugins: [react({ fastRefresh: false })],
+  plugins: [react({ fastRefresh: false })],
 });
